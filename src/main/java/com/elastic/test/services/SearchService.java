13af41aadc90faf408857f07ffb53f0871cdef69
Rodrigo Nievez <rodrigo.nievez@gmail.com>
2017-12-06 19:08:53 -0600

    Avance

---
+++ src/main/java/com/elastic/test/services/SearchService.java
@@ -0,0 +1,14 @@
+package com.elastic.test.services;
+
+import org.elasticsearch.index.query.BoolQueryBuilder;
+
+public class SearchService {
+
+    public void testingSearch() {
+
+    }
+
+    public BoolQueryBuilder generateQuery() {
+
+    }
+}
